31ec6cc6ab3a22897065b268aca9de83f68028c9
Ah3ron <ah3ron@protonmail.com>
2024-01-07 06:57:29 +0300

    Added VeryLazy event in cellular-automaton

--- lua/plugins/cellular-automaton.lua
+++ lua/plugins/cellular-automaton.lua
@@ -1,5 +1,6 @@
 return {
   "eandrju/cellular-automaton.nvim",
+  event = "VeryLazy",
   config = function()
     vim.keymap.set("n", "<leader>mir", "<cmd>CellularAutomaton make_it_rain<CR>")
     vim.keymap.set("n", "<leader>mig", "<cmd>CellularAutomaton game_of_life<CR>")
